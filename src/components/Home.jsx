99c47abd108f19cff935c334d3a3d39cb0136845
Vairamuthu M <mvairamuthu2003@gmail.com>
2024-06-30 20:22:28 +0530

    updated the hero section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,36 +1,29 @@
 import React from "react";
-import { ConnectButton } from "thirdweb/react";
-
-import { createWallet, walletConnect, inAppWallet } from "thirdweb/wallets";
-import { createThirdwebClient } from "thirdweb";
-
-const client = createThirdwebClient({
-  clientId: "bdf8ac5001cc48ecf7efeec38c34a82f",
-});
-
-const wallets = [
-  createWallet("io.metamask"),
-  createWallet("com.coinbase.wallet"),
-  walletConnect(),
-  inAppWallet({
-    auth: {
-      options: ["email", "google", "apple", "facebook", "phone"],
-    },
-  }),
-  createWallet("com.trustwallet.app"),
-  createWallet("me.rainbow"),
-];
+import Navbar from "./Navbar";
+import herobg from "../assets/hero-bg.svg";
 
 function Home() {
   return (
     <div>
-      Home
-      <ConnectButton
-        client={client}
-        wallets={wallets}
-        theme={"dark"}
-        connectModal={{ size: "compact" }}
-      />
+      <main className="min-h-screen bg-[url('../assets/bg.png')] bg-cover bg-center relative overflow-hidden">
+        <Navbar />
+        <div className="text-container">
+          <p className="herotxt">
+            A New Era &nbsp; &nbsp; &nbsp; &nbsp; Multiplayer
+          </p>
+          <p className="herotxt">Onchain &nbsp; &nbsp; &nbsp; Adventure</p>
+        </div>
+        <img src={herobg} alt="Hero Background" className="herobg" />
+        <div className="additional-content">
+          <p className="highlight-text">Pushing the Limits of Onchain Gaming</p>
+          <p className="description-text">
+            Immerse yourself in the future of gaming on the Base chain.
+            Experience epic PvP battles, earn exclusive NFT rewards, and embrace
+            gaming realism like never before.
+          </p>
+          <button className="playnowbtn">Play Now!!</button>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,62 @@
+import { ConnectButton, darkTheme } from "thirdweb/react";
+import { createWallet, walletConnect, inAppWallet } from "thirdweb/wallets";
+import { createThirdwebClient } from "thirdweb";
+import navicon from "../assets/navicon.png";
+
+const client = createThirdwebClient({
+  clientId: "bdf8ac5001cc48ecf7efeec38c34a82f",
+});
+
+const wallets = [
+  createWallet("io.metamask"),
+  createWallet("com.coinbase.wallet"),
+  walletConnect(),
+  inAppWallet({
+    auth: {
+      options: ["email", "google", "apple", "facebook", "phone"],
+    },
+  }),
+  createWallet("com.trustwallet.app"),
+  createWallet("me.rainbow"),
+];
+
+const customTheme = darkTheme({
+  colors: {
+    modalBg: "#FFFFFF",
+    primaryText: "black",
+    separatorLine: "#FFFFFF",
+    borderColor: "#EEEEEE",
+    accentText: "#c0ffee",
+    connectedButtonBg: "#B9FF09",
+    primaryButtonBg: "#B9FF09",
+  },
+});
+
+function Navbar() {
+  return (
+    <div>
+      <div className="flex items-center justify-between md:px-32 md:pt-20">
+        <div className="flex gap-20">
+          <p>How it works</p>
+          <p>Marketplace</p>
+        </div>
+        <img src={navicon} alt="logo" />
+        <div className="flex gap-20">
+          <button className="bg-[#B9FF09] rounded-full text-2xl px-12 py-4  font-semibold text-black ">
+            Play Now!
+          </button>{" "}
+          <div>
+            <ConnectButton
+              client={client}
+              wallets={wallets}
+              theme={customTheme}
+              connectModal={{ size: "compact" }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
